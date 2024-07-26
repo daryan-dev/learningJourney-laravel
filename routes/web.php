1de73e54144e27dfcc41e79e080f2b6aed536cf4
daryan-dev <daryandara004@gmail.com>
2024-07-26 01:48:29 -0900

    rounte and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\profileController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','welcome');
+Route::get('profile',[profileController::class,'index']);
+
